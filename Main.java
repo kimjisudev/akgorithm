11c5960e4fbd9f31e1c572612a37c2b9adc30e57
jisuKIM <114086659+kimjisudev@users.noreply.github.com>
2023-01-20 05:16:51 +0000

    230120

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    // Bigger
+    int a = 3;
+    int b = 2;
+
+    if (a > b) {
+      System.out.println(a + "가 큰 수 입니다.");
+    } else if (a == b) {
+      System.out.println("a와 b는 같습니다.");
+    } else {
+      System.out.println(b + "가 큰 수 입니다.");
+    }
+
+  }
+}
